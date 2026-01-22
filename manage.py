abb012b6928d72bda3e080956e162b3867282709
Ujil0 <yusuke1019upi@gmail.com>
2026-01-22 22:51:08 +0900

    Render用Ver.1

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    #Djangoを動かすリモコン
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokeguesser.settings')
     try:
         from django.core.management import execute_from_command_line
